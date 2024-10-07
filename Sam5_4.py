30d70378b717851e118711dba23c82184771b5a8
churchkhe1a <tryla1a@mail.ru>
2024-10-08 01:58:03 +0500

    Update Sam5_4.py

--- Sam5_4.py
+++ Sam5_4.py
@@ -1,14 +1,18 @@
-def main(*args):
+def fix_grades(grades):
+ new_grades = []
+ for grade in grades:
+  if grade == 2:
+   continue
+  elif grade == 3:
+   new_grades.append(4)
+  else:
+   new_grades.append(grade)
+ return new_grades
 
-    if not args:
-        return 0
+grades1 = [2, 3, 4, 5, 3, 4, 5, 2, 2, 5, 3, 4, 3, 5, 4]
+grades2 = [4, 2, 3, 5, 3, 5, 4, 2, 2, 5, 4, 3, 5, 3, 4]
+grades3 = [5, 4, 3, 3, 4, 3, 3, 5, 5, 3, 3, 3, 3, 4, 4]
 
-    total = sum(args)
-    average = total / len(args)
-    return average
-
-
-if __name__ == "__main__":
-    numbers = [1, 2, 3, 4, 5]
-    average = main(*numbers)
-    print(f"Среднее арифметическое: {average}")
+print(f"Обновленный список оценок 1: {fix_grades(grades1)}")
+print(f"Обновленный список оценок 2: {fix_grades(grades2)}")
+print(f"Обновленный список оценок 3: {fix_grades(grades3)}")
