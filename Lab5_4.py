9b71105caa8e3f543036ec7d8536db637b4bff5d
churchkhe1a <tryla1a@mail.ru>
2024-10-08 01:56:00 +0500

    Update Lab5_4.py

--- Lab5_4.py
+++ Lab5_4.py
@@ -1,17 +1,2 @@
-def main(x, *args):
-    one = x
-    two = sum(args)
-    three = float(len(args))
-
-    print(f"one = {one}\ntwo = {two}\nthree = {three}")
-
-    return x + sum(args) / float(len(args))
-
-if __name__ == '__main__':
-    result = main(5, 1, -2, 3, 4)
-    print(f"\nresult = {result}")
-
-# 1. x = 5 (первый аргумент)
-# 2. sum(args) = 1 + (-2) + 3 + 4 = 6 (сумма остальных аргументов)
-# 3. len(args) = 4 (количество остальных аргументов)
-# 4. 5 + 6 / 4 = 6.5 (результат вычисления)
+a = [12, 54, 32, 57, 843, 2346, 765, 75, 25, 234, 756, 23]
+print(a[2:6])
