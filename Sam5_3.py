26c10c6b9cbf2a27df733073aba30eebc6fae6af
churchkhe1a <tryla1a@mail.ru>
2024-10-08 01:57:52 +0500

    Update Sam5_3.py

--- Sam5_3.py
+++ Sam5_3.py
@@ -1,7 +1,18 @@
-import datetime
-import time
+import math
 
-for i in range(5):
-  current_time = datetime.datetime.now().strftime("%H:%M:%S")
-  print(f"Текущее время: {current_time}")
-  time.sleep(1)
+one = [12, 25, 3, 48, 71]
+two = [5, 18, 40, 62, 98]
+three = [4, 21, 37, 56, 84]
+
+max_sides = [max(one), max(two), max(three)]
+min_sides = [min(one), min(two), min(three)]
+
+def triangle_area(a, b, c):
+    s = (a + b + c) / 2
+    return math.sqrt(s * (s - a) * (s - b) * (s - c))
+
+area_max = triangle_area(*max_sides)
+area_min = triangle_area(*min_sides)
+
+print(f"Площадь треугольника из максимальных сторон: {area_max}")
+print(f"Площадь треугольника из минимальных сторон: {area_min}")
