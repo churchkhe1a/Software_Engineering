9f6d39fa13b09235a6ec9065c40779b2dce35b5e
churchkhe1a <tryla1a@mail.ru>
2024-10-15 21:11:44 +0500

    Update Sam6_4.py

--- Sam6_4.py
+++ Sam6_4.py
@@ -1,14 +1,23 @@
-def main(*args):
+def find_subsequence(logs, employee_id):
 
-    if not args:
-        return 0
+    first_index = -1
+    second_index = -1
 
-    total = sum(args)
-    average = total / len(args)
-    return average
+    for index, value in enumerate(logs):
+        if value == employee_id:
+            if first_index == -1:
+                first_index = index
+            elif second_index == -1:
+                second_index = index
+                break
+
+    if first_index == -1:
+        return ()
+    if second_index == -1:
+        return logs[first_index:]
+    return logs[first_index:second_index + 1]
 
 
-if __name__ == "__main__":
-    numbers = [1, 2, 3, 4, 5]
-    average = main(*numbers)
-    print(f"Среднее арифметическое: {average}")
+print(find_subsequence((1, 2, 3), 8))
+print(find_subsequence((1, 8, 3, 4, 8, 8, 9, 2), 8))
+print(find_subsequence((1, 2, 8, 5, 1, 2, 9), 8))
