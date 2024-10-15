c38a136b9895e257f6ecc9f4313df6527d6fdb22
churchkhe1a <tryla1a@mail.ru>
2024-10-15 21:14:30 +0500

    Update Lab6_2.py

--- Lab6_2.py
+++ Lab6_2.py
@@ -1,7 +1,12 @@
-def main():
-  result = 2 + 2
-  return result
+from pprint import pprint
 
-if __name__ == '__main__':
-  answer = main()
-  print(answer)
+my_dict = {'first': 'so easy'}
+
+
+def dict_maker(**kwargs):
+    my_dict.update(**kwargs)
+
+
+dict_maker(a1 = 1, a2 = 20, a3 = 54, a4 = 13)
+dict_maker(name = 'Михаил', age = 31, weight = 70, eyes_color = 'blue')
+pprint(my_dict)
