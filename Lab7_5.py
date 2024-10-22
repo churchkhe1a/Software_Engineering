c27c6b6db127d9809f335a04626d43616fba5a6f
churchkhe1a <tryla1a@mail.ru>
2024-10-23 03:20:39 +0500

    Update Lab7_5.py

--- Lab7_5.py
+++ Lab7_5.py
@@ -1,9 +1,3 @@
-def tuple_sort(tpl):
-    for elm in tpl:
-        if not isinstance(elm, int):
-            return tpl
-    return tuple(sorted(tpl))
-
-if __name__ == '__main__':
-    print(tuple_sort((5, 5, 3, 1, 9)))
-    print(tuple_sort((5, 5, 2.1, '1', 9)))
+with open('text.txt') as f:
+    for line in f:
+        print(line)
