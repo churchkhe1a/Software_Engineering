695809f3349d33b0712e4384ffc6510e84410881
churchkhe1a <tryla1a@mail.ru>
2024-10-23 03:23:40 +0500

    Update Sam7_4.py

--- Sam7_4.py
+++ Sam7_4.py
@@ -1,23 +1,17 @@
-def find_subsequence(logs, employee_id):
+import re
 
-    first_index = -1
-    second_index = -1
-
-    for index, value in enumerate(logs):
-        if value == employee_id:
-            if first_index == -1:
-                first_index = index
-            elif second_index == -1:
-                second_index = index
-                break
-
-    if first_index == -1:
-        return ()
-    if second_index == -1:
-        return logs[first_index:]
-    return logs[first_index:second_index + 1]
-
-
-print(find_subsequence((1, 2, 3), 8))
-print(find_subsequence((1, 8, 3, 4, 8, 8, 9, 2), 8))
-print(find_subsequence((1, 2, 8, 5, 1, 2, 9), 8))
+def load_banned_words(file_path):
+    with open(file_path, 'r', encoding='utf-8') as file:
+        return [line.strip() for line in file.readlines()]
+def replace_banned_words(sentence, banned_words):
+    for word in banned_words:
+        pattern = re.compile(re.escape(word), re.IGNORECASE)
+        sentence = pattern.sub('*' * len(word), sentence)
+    return sentence
+def main():
+    banned_words = load_banned_words('text.txt')
+    sentence = "Hello, world! Python IS the programming language of thE future. My EMAIL is.... PYTHON is awesome!!!!"
+    result = replace_banned_words(sentence, banned_words)
+    print("Результат:", result)
+if __name__ == "__main__":
+    main()
