79cc7449193ff881fbd0804c51177f063e2891de
churchkhe1a <tryla1a@mail.ru>
2024-10-23 03:22:48 +0500

    Update Sam7_1.py

--- Sam7_1.py
+++ Sam7_1.py
@@ -1,8 +1,17 @@
-user_input = input("Введите последовательность чисел, разделенных пробелом: ")
+from collections import Counter
+import re
 
-numbers_list = [int(x) for x in user_input.split()]
 
-numbers_tuple = tuple(numbers_list)
-
-print("Список: ", numbers_list)
-print("Кортеж: ", numbers_tuple)
+def analyze_text_file(directory):
+    try:
+        with open(directory, 'r', encoding='utf-8') as file:
+            text = file.read()
+        words = re.findall(r'\b\w+\b', text.lower())
+        word_count = len(words)
+        word_frequency = Counter(words)
+        most_common_word, most_common_count = word_frequency.most_common(1)[0]
+        print(f"Количество слов в файле: {word_count}")
+        print(f"Самое часто встречающееся слово: '{most_common_word}' (встречается {most_common_count} раз)")
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+analyze_text_file(r'C:\Users\Aleks\PycharmProjects\Tema7\bananacat.txt')
