5004a2e4992feab262a163c1f54aa50b6d42acb5
churchkhe1a <tryla1a@mail.ru>
2024-10-23 03:20:22 +0500

    Update Lab7_4.py

--- Lab7_4.py
+++ Lab7_4.py
@@ -1,9 +1,2 @@
-def personal_info(name, age, company = 'unnamed'):
-    print(f"Имя: {name} Возраст: {age} Компания: {company}")
-
-
-tom = ("Григорий", 22)
-personal_info(*tom)
-
-bob = ("Георгий", 41, "Yandex")
-personal_info(*bob)
+with open('text.txt') as f:
+    print(f.readlines())
