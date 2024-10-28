915d63b3010beceaaf6111a4e0eb29aa0fcd26c7
churchkhe1a <tryla1a@mail.ru>
2024-10-29 01:19:33 +0500

    Update Lab8_5.py

--- Lab8_5.py
+++ Lab8_5.py
@@ -1,9 +1,23 @@
-def tuple_sort(tpl):
-    for elm in tpl:
-        if not isinstance(elm, int):
-            return tpl
-    return tuple(sorted(tpl))
+class Shape: # определение абстрактного класса Shape (базовый класс)
+  def area(self): # метод area, который должен быть переопределен в дочерних классах
+    pass # пока что ничего не делает
 
-if __name__ == '__main__':
-    print(tuple_sort((5, 5, 3, 1, 9)))
-    print(tuple_sort((5, 5, 2.1, '1', 9)))
+class Rectangle(Shape): # определение класса Rectangle, наследующего от класса Shape
+  def __init__(self, width, height): # метод инициализации класса Rectangle
+    self.width = width # создание атрибута width и присваивание ему значения аргумента width
+    self.height = height # создание атрибута height и присваивание ему значения аргумента height
+
+  def area(self): # переопределение метода area для класса Rectangle
+    return self.width * self.height # возвращает площадь прямоугольника
+
+class Circle(Shape): # определение класса Circle, наследующего от класса Shape
+  def __init__(self, radius): # метод инициализации класса Circle
+    self.radius = radius # создание атрибута radius и присваивание ему значения аргумента radius
+
+  def area(self): # переопределение метода area для класса Circle
+    return 3.14 * self.radius * self.radius # возвращает площадь круга
+
+shapes = [Rectangle(5, 4), Circle(3)] # создание списка shapes, содержащего объекты классов Rectangle и Circle
+
+for shape in shapes: # цикл for для перебора объектов в списке shapes
+  print(f"The area is: {shape.area()}") # вывод площади каждого объекта с использованием метода area()
