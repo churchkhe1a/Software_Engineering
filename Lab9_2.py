8bceb6398115bfda749f5aed6e60836bbc815310
churchkhe1a <tryla1a@mail.ru>
2024-11-04 17:31:18 +0500

    Update Lab9_2.py

--- Lab9_2.py
+++ Lab9_2.py
@@ -1,12 +1,19 @@
-from pprint import pprint
+class Icecream:
+    def __init__(self, ingredient=None):
+        if isinstance(ingredient, str):
+            self.ingredient = ingredient
+        else:
+            self.ingredient = None
 
-my_dict = {'first': 'so easy'}
-
-
-def dict_maker(**kwargs):
-    my_dict.update(**kwargs)
-
-
-dict_maker(a1 = 1, a2 = 20, a3 = 54, a4 = 13)
-dict_maker(name = 'Михаил', age = 31, weight = 70, eyes_color = 'blue')
-pprint(my_dict)
+    def composition(self):
+        if self.ingredient:
+            print(f"Мороженое с {self.ingredient}")
+        else:
+            print('Обычное мороженое')
+            
+icecream = Icecream()
+icecream.composition()
+icecream = Icecream('шоколадом')
+icecream.composition()
+icecream = Icecream(5)
+icecream.composition()
