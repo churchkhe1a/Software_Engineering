48e79dfb19c2d7fd319053d7886f56d6e6598a79
churchkhe1a <tryla1a@mail.ru>
2024-11-06 15:38:10 +0500

    Update Sam9_2.py

--- Sam9_2.py
+++ Sam9_2.py
@@ -1,14 +1,1 @@
-def remove_first_occurrence(tpl, value):
-    lst = list(tpl)
 
-    try:
-        lst.remove(value)
-    except ValueError:
-        pass
-
-    return tuple(lst)
-
-
-print(remove_first_occurrence((1, 2, 3), 1))
-print(remove_first_occurrence((1, 2, 3, 1, 2, 3, 4, 5, 2, 3, 4, 2, 4, 2), 3))
-print(remove_first_occurrence((2, 4, 6, 6, 4, 2), 9))
