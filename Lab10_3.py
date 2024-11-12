485da8a0ffeea69e6d235d09a3fd04adebc96c67
churchkhe1a <tryla1a@mail.ru>
2024-11-13 03:49:43 +0500

    Update Lab10_3.py

--- Lab10_3.py
+++ Lab10_3.py
@@ -1,31 +1,15 @@
-class MyClass:
-    def __init__(self, value):
-        self._value = value
+def data(*args):
+    try:
+        for i in range(len(*args)):
+            try:
+                result = (args[0][i] *15)//10
+                print(result)
+            except Exception as ex:
+                print(ex)
+    except Exception as ex:
+        print(ex)
+    finally:
+        print('Вся информация обработана')
 
-    def set_value(self, value):
-        self._value = value
-
-    def get_value(self):
-        if hasattr(self, '_value'):
-            return self._value
-        else:
-            raise AttributeError("Значение удалено")
-
-    def del_value(self):
-        del self._value
-
-    value = property(get_value, set_value, del_value, "Свойство value")
-
-
-obj = MyClass(42)
-print(obj.value)
-obj.set_value(45)
-print(obj.value)
-obj.set_value(100)
-print(obj.value)
-obj.del_value()
-try:
-    print(obj.value)
-except AttributeError as e:
-    print(e)
-# Проблема была в том что метод get_value принимал value, и в том что после удаления value, мы пытались вывести value без проверки его удаления.
+if __name__ == '__main__':
+    data([1,15, 'Hello', 'i', 'try', 'to', 'crash', 'your','site', 38, 45])
