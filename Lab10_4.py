df7af2e62d8b54913033133097eba1cbf33f1351
churchkhe1a <tryla1a@mail.ru>
2024-11-13 03:50:20 +0500

    Update Lab10_4.py

--- Lab10_4.py
+++ Lab10_4.py
@@ -1,18 +1,12 @@
-class Mammal:
-    className = 'Mammal'
+class NegativeValueException(Exception):
+    pass
 
+def check_name(name):
+    if len(name)>10:
+        raise NegativeValueException('Длина более 10 символов')
+    else:
+        print('Успешная регистрация')
 
-class Dog(Mammal):
-    species = 'canine'
-    sounds = 'wow'
-
-
-class Cat(Mammal):
-    species = 'feline'
-    sounds = 'meow'
-
-
-dog = Dog()
-print(f"Dog is {dog.className}, but they say {dog.sounds}")
-cat = Cat()
-print(f"Cat is {cat.className}, but they say {cat.sounds}")
+if __name__ == '__main__':
+    name = 'SashaOblasova'
+    check_name(name)
